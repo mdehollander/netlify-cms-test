907a7fd285d6c7661feaf8595c635271ad1b23dd
Mattias de Hollander <mattias@biomatt.nl>
2017-09-02 23:59:03 +0200

    Only transfer files that changed in size

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
-lftp -c "set ftps:initial-prot ''; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21/biomatt.nl/DEFAULT/; mirror -Rv public .; quit;"
+lftp -c "set ftps:initial-prot ''; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21/biomatt.nl/DEFAULT/; mirror --verbose --reverse --only-newer --ignore-time public .; quit;"
